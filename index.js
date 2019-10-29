283e628f3574c4c6eac013b1ba225f9d0b6fa9a1
Renato Siqueira <renatoelysiqueira@gmail.com>
2019-10-29 01:49:03 -0300

    Assets Added and Database Methods

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const express = require('express')
 const app = express()
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
 
 const port = process.env.PORT || 3000
+let database
 
 app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
+app.get('/', (req, res) => {
+    res.render('index')
+})
 
-app.listen(port, () => console.log('iRango Server Running...'))
+const insert = (db, collectioName, doc) => {
+    return new Promise((resolve, reject) => {
+        const collection = db.collection(collectioName)
+        collection.insert(doc, (err, result) => {
+            if (err)
+                reject(err)
+            else
+                resolve(result)
+        })
+    })
+}
+
+MongoClient.connect('mongodb://localhost:27017/irango', (err, db) => {
+    database = db
+    app.listen(port, () => console.log('iRango Server Running...'))
+})
